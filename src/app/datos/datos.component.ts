29a127f52432dc377a95b2120209701bd4f20303
Jonnathan Campoberde <jonnathanca9601@gmail.com>
2019-01-08 00:52:15 -0500

    Sumatoria wats

--- src/app/datos/datos.component.ts
+++ src/app/datos/datos.component.ts
@@ -8,6 +8,7 @@ import { PeticionServicio } from '../service';
 })
 export class DatosComponent implements OnInit {
   public datos: {};
+  public consumo: number;
 
   constructor(
     private _peticionesServices: PeticionServicio
@@ -16,11 +17,23 @@ export class DatosComponent implements OnInit {
   ngOnInit() {
     this._peticionesServices.getDatos().subscribe(
       result => {
-        console.log(result);
+        this.datos = result['feeds'];
+        console.log(this.datos);
         console.log('Se recupero los datos de una manera correcta');
-    },
+        this.mostrarConsumo()
+      },
     error => {
       console.log(<any>error);
     });
+  }
+
+  mostrarConsumo(){
+    var suma = 0;
+    for ( var i = 0 ; i < this.datos.length; i++) {
+      if ( this.datos[i]['field1'] != null){
+        suma += Number(this.datos[i]['field1']);
+        console.log(suma)
+      }
+  }
   }
 }
